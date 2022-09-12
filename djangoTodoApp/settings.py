f84e9ecb7708bd91f2a59e2373ae560344bf4d6b
Muhammad Shamshad <73699937+shamshadlive@users.noreply.github.com>
2022-09-12 11:48:19 +0530

    Update settings.py

--- djangoTodoApp/settings.py
+++ djangoTodoApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#'
-#django-insecure-df0&hhj7#si9f@l@1hpuh^2_!t17a#!r0+ro6v0%by=d=zn$hw
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
